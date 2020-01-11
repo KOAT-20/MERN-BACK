0e2a3094ee52a9d2e06cbb625ad6fad1ae6b9633
Kevin <keviinnalvarez2015@gmail.com>
2020-01-11 15:04:41 -0400

    separando las rutas, para un mejor manejo

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 4000);
 app.use(cors());
 app.use(express.json());
 // Routes
-app.get('/api/users', (req, res) => res.send('Users of Routes'))
-app.get('/api/notes', (req, res) => res.send('Notes Routes'))
+app.use('/api/users', require('./routes/users.js'));
+app.use('/api/notes', require('./routes/notes.js'));
 
 module.exports = app;
